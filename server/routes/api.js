8977df8b9321ac6c238bce6d5436617095a9812c
Luis <delapenl@gmail.com>
2015-09-28 12:49:40 -0600

    routes are all set up

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,4 +2,81 @@ var express = require('express');
 var router = express.Router();
 var Tatum = require('../models/tatum.js');
 
+
+// get all tatums
+router.get('/tatums', function(req, res, next) {
+  Tatum.findQ()
+    .then(function(results){
+      res.json(results);
+    }).catch(function(results){
+      res.json({'message': results});
+    }).done();
+});
+
+
+
+// post ALL tatums
+router.post('/tatums', function(req, res, next) {
+  newTatum = new Tatum({
+    movie: req.body.movie,
+    year: req.body.year,
+    chickflick: req.body.chickflick
+  }).saveQ()
+    .then(function(results){
+      res.json(results);
+    }).catch(function(results){
+      res.json({'message': results});
+    }).done();
+});
+
+
+
+
+
+// get SINGLE tatums
+router.get('/tatum/:id', function(req, res, next) {
+  Tatum.findByIdQ(req.params.id)
+  .then(function(results) {
+    res.json(results);
+  }).catch(function(results){
+    res.json({'message': results});
+  }).done();
+});
+
+
+
+// put SINGLE tatum
+router.put('/tatum/:id', function(req, res, next) {
+    var update = {
+    movie: req.body.movie,
+    year: req.body.year,
+    chickflick: req.body.chickflick
+  };
+  Tatum.findByIdAndUpdateQ(req.params.id, update)
+  .then(function(results){
+    res.json(results);
+  }).catch(function(results) {
+    res.json({'message':results});
+  }).done();
+});
+
+
+
+
+// delete SINGLE tatum
+router.delete('/tatum/:id', function(req, res, next) {
+  Tatum.findByIdAndRemoveQ(req.params.id)
+  .then(function(results){
+    res.json(results);
+  }).catch(function(results) {
+    res.json({'message': results});
+  }).done();
+});
+
+
+
+
+
+
+
 module.exports = router;
